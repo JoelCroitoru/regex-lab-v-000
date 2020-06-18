a46cbd1efaf125d60d4e58b70140392f8feab118
Joel Croitoru <jcroitoru@gmail.com>
2020-06-18 13:50:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\b[0-9]{10}\b/)
+  if phone.match(/\b[0-9]{10}\b|\b[([0-9]{3})-[0-9]{3}-[0-9]{4}]\b/)
     return true
   else
     return false
